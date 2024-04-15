7d705f242daafeae0f39de8119516c0be26846ac
Mehak <mehakatra496@gmail.com>
2024-04-15 11:20:34 +0530

    one

---
+++ Arrays/A1_Arr.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arrays;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class A1_Arr {
+    public static void main(String[] args) {
+        
+//        int[] arr1={ 23, 56 , 1,78,90 };
+        
+//        for(int i=0; i<arr1.length; i++)
+//        {
+//            System.out.println(arr1[i]);
+//        }
+     
+//        int[] arr2=new int[25];
+//               
+//        arr2[0]=34;
+//        arr2[1]=90;
+//        for(int i=0; i<arr2.length; i++)
+//        {
+//            System.out.println(arr2[i]);  
+//        }
+
+//        String[] st=new String[3];
+//        
+//        st[1]="Mehak";
+//        st[2]="kkk";
+//        st[0]="pow";
+//        
+//           for(int i=0; i<st.length; i++)
+//            {
+//                System.out.println(st[i]);  
+//            }
+
+//boolean[] bb=new boolean[5];
+//
+//bb[0]=true;
+//bb[1]=true;
+//
+//bb[2]=false;
+
+//bb[3]=true;
+//        for(int i=0; i<bb.length; i++)
+//        System.out.println(bb[i]);
+   
+
+//    char[] ch=new char[3];
+//    ch[1]='s';
+//    ch[2]='g';
+//    ch[3]='k';
+
+//    char[] ch2={34,56,66, 'k'};
+//
+//            for(int i=0; i<ch2.length; i++)
+//                System.out.println((char)ch2[i]);   //output: "", 8, B
+    
+    
+//        Object obj[]= {"mhk", new Integer(1)};
+//	System.out.println( obj[0]);
+//	System.out.println( obj[1]);
+
+        int[][] arr= new int[2][3];
+        int[][] arr2= {{34,99,22},{56,67,33}};
+        
+        for(int i=0; i<2; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                System.out.print(arr2[i][j]);
+            }
+            System.out.println("\n");
+        }
+        
+         }
+
+    }
